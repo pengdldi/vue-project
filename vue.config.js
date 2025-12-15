e0575c1772382ed965a0524ef44eadd159bda62c
pengdldi <imcjw93@gmail.com>
2025-12-16 01:25:44 +0900

    vue.config.js 수정

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = { chinWeppack: config => {
+  config.plugins.delete('prefetch'); //prefetch삭제
+}}
